44b9b74e482287cc2bfe2e593b14834a495e710f
bleonardb3 <bleonardb3@gmail.com>
2019-02-06 10:58:25 -0500

    Add files via upload

--- Lab-3/connection.R
+++ Lab-3/connection.R
@@ -2,20 +2,13 @@
 
 # Fill in connection details about your DB2 Warehouse instance below.
 
-#dsn.hostname <- 'HOSTNAME HERE'
-#dsn.uid <- 'USER ID HERE'
-#dsn.pwd <- 'PASSWORD HERE'
-#dsn.database <- 'BLUDB'
-#dsn.port <- '50000'
-#dsn.protocol <- 'TCPIP'
-
-#vetting.table = 'VETTING TABLE NAME HERE'
-
-dsn.hostname <- 'dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net'
-dsn.uid <- 'xvd96032'
-dsn.pwd <- 'c1mdg0gxrt-rj0wt'
+dsn.hostname <- 'HOSTNAME HERE'
+dsn.uid <- 'USER ID HERE'
+dsn.pwd <- 'PASSWORD HERE'
 dsn.database <- 'BLUDB'
 dsn.port <- '50000'
 dsn.protocol <- 'TCPIP'
 
-vetting.table = 'FEMALE_HUMAN_TRAFFICKING'
+vetting.table = 'VETTING TABLE NAME HERE'
+
+
